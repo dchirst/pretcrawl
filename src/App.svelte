f8430a48a070d57b2216006c9b0c6767f0553b31
dhirst <dan.hirst@os.uk>
2022-06-08 10:39:37 +0100

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let name: string;
+	// @ts-ignore
+	const apiKey = OS_API_KEY;
+</script>
+
+<main>
+	<h1 class="font-mono text-xl">Hello {name}</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
+</style>
