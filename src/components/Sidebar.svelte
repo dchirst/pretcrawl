7a92f35af7b14b6328bb2b76a684d88cbbce9bc7
dhirst <dan.hirst@os.uk>
2022-06-11 23:32:02 +0100

    Added osdatahubauth test onto the svelte app

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,8 +1,17 @@
 <script>
     import Logo from "./Logo.svelte";
+
+    let value;
+    fetch("/api/OsdatahubAuth", {
+        method: "POST",
+        data: JSON.stringify({
+    key: "mgz99Gos1K50W1SpyQEOUqFujOxKpqyz"
+})
+    }).then(response => response.json()).then(response => value = response)
 </script>
 
 
 <div class="w-96 h-screen">
     <Logo/>
+    <p>Data is {value}</p>
 </div>
