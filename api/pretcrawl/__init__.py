c9b72d9de6cae3467ce5529a02d1ea0adda55fc4
dhirst <dan.hirst@os.uk>
2022-06-11 22:17:20 +0100

    Added function framework

---
+++ api/pretcrawl/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. PRET CRAWL.")
+    else:
+        return func.HttpResponse(
+             "PRET CRAWL",
+             status_code=200
+        )
