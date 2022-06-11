c4da69b1698d56968830bc102a86dbf710fa9b23
dhirst <dan.hirst@os.uk>
2022-06-11 20:35:36 +0100

    Implemented osdatahubs OAuth API in Azure Function

--- api/OsdatahubAuth/__init__.py
+++ api/OsdatahubAuth/__init__.py
@@ -1,9 +1,31 @@
 import logging
+import requests
+from os import environ
+import json
 
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    data = json.loads(req.get_json())
+    logging.info(data)
+    key = data["key"]
+    secret = environ.get("OS_API_SECRET")
+    logging.info(secret)
+    auth = (key, secret)
+    timeout = 200
 
-    return func.HttpResponse("API_KEY", status_code=200)
+    response = requests.post(
+        url='https://api.os.uk/oauth2/token/v1',
+        auth=auth,
+        data={
+            "grant_type": "client_credentials"
+        }
+    )
+    logging.info(response.content)
+
+    return func.HttpResponse(
+        json.dumps(response.content.decode("utf-8")),
+        status_code=200
+    )
