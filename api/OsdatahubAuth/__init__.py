37b909d270d2d36f8f6b9d7015ef47304fc53c20
dhirst <dan.hirst@os.uk>
2022-06-11 21:40:50 +0100

    Added basic error handling as a test

--- api/OsdatahubAuth/__init__.py
+++ api/OsdatahubAuth/__init__.py
@@ -7,30 +7,33 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    data = req.get_json()
-    logging.info(data)
-    key = data["key"]
-    secret = environ.get("OS_API_SECRET")
-    # logging.info(secret)
-    auth = (key, secret)
+    try:
+        logging.info('Python HTTP trigger function processed a request.')
+        data = req.get_json()
+        logging.info(data)
+        key = data["key"]
+        secret = environ.get("OS_API_SECRET")
+        # logging.info(secret)
+        auth = (key, secret)
 
-    response = requests.post(
-        url='https://api.os.uk/oauth2/token/v1',
-        auth=auth,
-        data={
-            "grant_type": "client_credentials"
-        }
-    )
-    logging.info(response.content)
-
-    if response.status_code == 200:
-        return func.HttpResponse(
-            json.dumps(response.content.decode("utf-8")),
-            status_code=200
+        response = requests.post(
+            url='https://api.os.uk/oauth2/token/v1',
+            auth=auth,
+            data={
+                "grant_type": "client_credentials"
+            }
         )
-    else:
+        logging.info(response.content)
+
+        if response.status_code == 200:
+            return func.HttpResponse(
+                json.dumps(response.content.decode("utf-8")),
+                status_code=200
+            )
+        else:
+            raise Exception("Something has gone wrong")
+    except Exception as e:
         return func.HttpResponse(
-            response.content.decode("utf-8"),
+            e,
             status_code=response.status_code
         )
