9dde76a65db74bbc8714e4b92e95489cdfd37f6f
dhirst <dan.hirst@os.uk>
2022-06-11 21:11:37 +0100

    Added minor error handling to oauth server

--- api/OsdatahubAuth/__init__.py
+++ api/OsdatahubAuth/__init__.py
@@ -8,13 +8,12 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    data = json.loads(req.get_json())
+    data = req.get_json()
     logging.info(data)
     key = data["key"]
     secret = environ.get("OS_API_SECRET")
-    logging.info(secret)
+    # logging.info(secret)
     auth = (key, secret)
-    timeout = 200
 
     response = requests.post(
         url='https://api.os.uk/oauth2/token/v1',
@@ -25,7 +24,13 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     )
     logging.info(response.content)
 
-    return func.HttpResponse(
-        json.dumps(response.content.decode("utf-8")),
-        status_code=200
-    )
+    if response.status_code == 200:
+        return func.HttpResponse(
+            json.dumps(response.content.decode("utf-8")),
+            status_code=200
+        )
+    else:
+        return func.HttpResponse(
+            response.content.decode("utf-8"),
+            status_code=response.status_code
+        )
