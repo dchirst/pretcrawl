23a4c6a70cd70d38f224bd55377ee31124d2078e
dhirst <dan.hirst@os.uk>
2022-06-12 10:01:06 +0100

    Implemented API key into Map

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -3,18 +3,6 @@ import { onMount } from "svelte";
 
     import Logo from "./Logo.svelte";
 
-    let value;
-    onMount(async () => {
-        await fetch("/api/OsdatahubAuth", {
-        method: "POST",
-        body: JSON.stringify({
-        key: process.env.OS_API_KEY
-    })
-    }).then(response => response.json()).then(response => value = response)
-    })
-
-    $: value = value
-
 </script>
 
 
