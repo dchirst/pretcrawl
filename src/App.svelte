b09df81f34a9262a5402201baaa3ccbd6bcba8c7
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-10-06 17:59:01 +0100

    Added basic SEO

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,36 @@
 	import Map from "./lib/Map.svelte";
 	import Sidebar from "./lib/Sidebar.svelte";
     import Spinner from "./lib/Spinner.svelte";
+    import SvelteSeo from "svelte-seo";
 
 </script>
+
+<SvelteSeo
+  title="Pret Crawl"
+  description="Pret Crawl is a tool for those who love to walk with a coffee in their hand.
+    Simply choose your route, and the Pret Crawl will find the optimal Pret A Manger's to visit throughout your journey",
+  keywords="Pret,Pret A Manger, Map, Crawl"
+  openGraph={{
+    title: 'Make your own Pret Crawl',
+    description: 'Forever walk through London with a Pret in your hand',
+    url: 'https://mango-sky-05b77640f.2.azurestaticapps.net/',
+    type: 'website',
+    images: [
+      {
+        url: 'https://raw.githubusercontent.com/dchirst/pretcrawl/master/media/pretcrawl-logo.jpg',
+        width: 850,
+        height: 650,
+        alt: 'Pret Crawl Logo'
+      },
+      {
+        url: 'https://raw.githubusercontent.com/dchirst/pretcrawl/master/media/app-screenshot.png',
+        width: 850,
+        height: 650,
+        alt: 'Pret Crawl Screenshot'
+      }
+     ]
+  }}
+/>
 
 <Sidebar/>
 <Map/>
