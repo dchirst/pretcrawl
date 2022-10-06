95cf1c88edc833f1567d6e52db34b322323cdb51
Dan Hirst <93535587+dchirst@users.noreply.github.com>
2022-10-06 15:20:18 +0100

    Style improvements

---
+++ src/lib/Logo.svelte
@@ -0,0 +1,14 @@
+    <div class="mx-auto w-auto">
+      <h1 class="text-xxl align-middle inline-block">PRET</h1>
+    <img class="my-auto inline-block w-16" src="star.png" alt="star"/>
+    <h1 class="text-xxl align-middle inline-block">CRAWL</h1>
+    </div>
+
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Truculenta:opsz,wght@12..72,500&display=swap');
+h1 {
+    font-family: 'Truculenta', sans-serif;
+}
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,28 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
+
     extend: {},
   },
 
   plugins: [require("daisyui")],
+      daisyui: {
+      themes: [
+        {
+          mytheme: {
+           "primary": "rgb(132, 24, 43)",
+           "secondary": "#b91c1c",
+           "accent": "#fca5a5",
+           "neutral": "#242B38",
+           "base-100": "#FCFCFD",
+           "info": "#7BA4EA",
+           "success": "#19C867",
+           "warning": "#D9B517",
+           "error": "#EE494F",
+          },
+        },
+      ],
+    },
 };
 
 module.exports = config;
